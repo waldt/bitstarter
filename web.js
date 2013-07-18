38d05b6ead0c404dd1398add18e12bb35bd23f4e
Jaan Angerpikk <jaan.angerpikk@gmail.com>
2013-07-18 14:11:58 +0300

    wrapped async fs.ReadFile

--- web.js
+++ web.js
@@ -1,16 +1,23 @@
 var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
-fs.readFile("index.html", function (err, data) {
+
+var readContentFromFile = function(callback) {
+    fs.readFile("index.html", function (err, data) {
         if (err) {
             throw err
         }
-        var greeting = data.toString();
-        app.get('/', function (request, response) {
-            response.send(greeting);
-        });
-    }
-);
+        callback(data);
+    })
+};
+
+readContentFromFile(function(data) {
+    var greeting = data.toString();
+    app.get('/', function (request, response) {
+        response.send(greeting);
+    });
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Listening on " + port);
